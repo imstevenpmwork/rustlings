f807370d04a6f46d4245d256390f155561b60bf7
Steven Palma <imstevenpmwork@ieee.org>
2022-10-25 22:57:09 +0200

    up to clippy

--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -2,8 +2,6 @@
 // Execute `rustlings hint threads1` or use the `hint` watch subcommand for a hint.
 // This program should wait until all the spawned threads have finished before exiting.
 
-// I AM NOT DONE
-
 use std::thread;
 use std::time::Duration;
 
@@ -11,16 +9,23 @@ use std::time::Duration;
 fn main() {
 
     let mut handles = vec![];
-    for i in 0..10 {
-        thread::spawn(move || {
-            thread::sleep(Duration::from_millis(250));
+    for i in 0..5 {
+        handles.push(thread::spawn(move || {
+            thread::sleep(Duration::from_millis(1000));
             println!("thread {} is complete", i);
-        });
+        }));
+    }
+    for i in 6..11 {
+        handles.push(thread::spawn(move || {
+            thread::sleep(Duration::from_millis(500));
+            println!("thread {} is complete", i);
+        }));
     }
 
     let mut completed_threads = 0;
     for handle in handles {
         // TODO: a struct is returned from thread::spawn, can you use it?
+        handle.join();
         completed_threads += 1;
     }
 
